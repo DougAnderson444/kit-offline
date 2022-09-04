e6b0250bf682a03e32e2905ef112fa10527b7956
Doug A <douganderson444@gmail.com>
2022-09-04 16:47:13 -0300

    cahce static files too

--- src/service-worker/installEvent.ts
+++ src/service-worker/installEvent.ts
@@ -1,5 +1,5 @@
 import { CACHE_NAME } from './constants';
-import { build } from '$service-worker';
+import { build, files } from '$service-worker';
 
 export default (event: ExtendableEvent): void => {
 	console.log('installing service worker');
@@ -8,6 +8,7 @@ export default (event: ExtendableEvent): void => {
 		caches.open(CACHE_NAME).then((cache) => {
 			// Open a cache and cache our files
 			cache.addAll(build);
+			cache.addAll(files);
 
 			return true;
 		})
